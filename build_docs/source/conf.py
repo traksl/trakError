d008c2d46713af3ed910d8937df29dae3ccd7dea
Daniel Soler <daniel.soler@nostrumbiodiscovery.com>
2021-10-21 12:13:39 +0000

    deploy: aaeef16caefa7f57bb07f3afad1ac200224a5c8d

--- build_docs/source/conf.py
+++ build_docs/source/conf.py
@@ -58,3 +58,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_theme_options = {
+'prev_next_buttons_location': None
+}
+html_show_sourcelink = False
